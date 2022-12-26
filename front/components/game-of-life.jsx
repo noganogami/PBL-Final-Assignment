616b2db0904a772074ec5a2bde4e84bff9b3dcae
noganogami <hydetkc7@gmail.com>
2022-12-27 05:00:31 +0900

    When click cell, cell's state is inverted.

--- front/components/game-of-life.jsx
+++ front/components/game-of-life.jsx
@@ -2,53 +2,51 @@ import React, { useState } from 'react';
 import {HStack, VStack, GridItem, SimpleGrid, Box, Text, Button, ButtonGroup} from '@chakra-ui/react'
 import Cell from './hello_life';
 
+// init
+let init = new Array(32);
+for(let i = 0; i < 32; i++) {
+   init[i] = new Array(32).fill(false);
+}
+for (let y = 0; y < 32; y++) {
+   for (let x = 0; x < 32; x++) {
+      if (y == 0 + 15 && x == 1 + 15) {
+         init[y][x] = true;
+      } else if (y == 0 + 15 && x == 2 + 15) {
+         init[y][x] = true;
+      } else if (y == 1 + 15 && x == 0 + 15) {
+         init[y][x] = true;
+      } else if (y == 2 + 15 && x == 1 + 15) {
+         init[y][x] = true;
+      }
+   }
+}
+let cell = new Cell(init);
+
 export default function LifeGame() {
-   // init
-   let init = new Array(32);
-   for(let i = 0; i < 32; i++) {
-      init[i] = new Array(32).fill(false);
-   }
-   for (let y = 0; y < 32; y++) {
-      for (let x = 0; x < 32; x++) {
-         if (y == 0 + 15 && x == 1 + 15) {
-            init[y][x] = true;
-         } else if (y == 0 + 15 && x == 2 + 15) {
-            init[y][x] = true;
-         } else if (y == 1 + 15 && x == 0 + 15) {
-            init[y][x] = true;
-         } else if (y == 2 + 15 && x == 1 + 15) {
-            init[y][x] = true;
-         }
-      }
-   }
+   const [cellState, setState] = useState(cell.current_state);
 
-   let cell = new Cell(init);
-   const [cellState, setState] = useState(cell.state);
 
-   function clickHandler() {
-      while (1) {
-         printCell();
-         cell.updateState();
-         setState(cell.current_state)
-      }
-
-   }
-
-   function invertState() {
-      setState(cellState.map((state) => !state))
+   function clickCell(event) {
+      const coordinate = event.currentTarget.id.split(',').map((s) => Number(s))
+      cell.invertState(coordinate[0], coordinate[1]);
+      setState(cell.current_state)
    }
 
    return (
       <Box>
-         {
-            cellState.map(states => <HStack spacing={0}> {states.map(state => {
-               if ( state ) {
-                  return <Box as='button' w={5} h={5} bg='black' borderWidth='1px'></Box>
+         <Box>
+            {
+               cellState.map((states, iy) => <HStack spacing={0}> {states.map((state, ix) => {
+                  if ( state ) {
+                     return <Box as='button' onClick={(e) => clickCell(e)} w={5} h={5} id={Array.from([ix,iy])} bg='black' borderWidth='1px'></Box>
 
-               } else {
-                  return <Box as='button' w={5} h={5} bg='white' borderWidth='1px'></Box>
-               }})} </HStack>)
-         }
+                  } else {
+                     return <Box as='button' onClick={(e) => clickCell(e)} w={5} h={5} id={Array.from([ix,iy])} bg='white' borderWidth='1px'></Box>
+                  }})} </HStack>)
+            }
+         </Box>
+         <Button >start</Button>
+         <Button >stop</Button>
       </Box>
    );
 }
--- front/components/hello_life.js
+++ front/components/hello_life.js
@@ -3,22 +3,42 @@ export default class Cell {
       this.current_state = initial_state; // array[32][32]
    }
 
-   get state() {
-      return this.current_state;
+
+   invertState(xi, yi) {
+      let next_state = new Array(32);
+      for(let i = 0; i < 32; i++) {
+         next_state[i] = new Array(32);
+      }
+
+      for (let y = 0; y < 32; y++) {
+         for (let x = 0; x < 32; x++) {
+            if (x == xi && y == yi) {
+               next_state[y][x] = !this.current_state[y][x];
+            } else {
+               next_state[y][x] = this.current_state[y][x];
+            }
+         }
+      }
+      this.current_state = next_state;
    }
 
+
    judgeState() {
-      let next_state;
+      let next_state = new Array(32);
+      for(let i = 0; i < 32; i++) {
+         next_state[i] = new Array(32);
+      }
+
       let count;
       for (let y = 0; y < 32; y++) {
          for (let x = 0; x < 32; x++) {
             count = 0;
-            for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
                // yi : -1,-1,-1,0,0,0,1,1,1
                // xi : -1,0,1,-1,0,1,-1,0,1
                // mask 0b11111 for edge handring
-               if (this.current_state[(32 - 1) & (y - 1 + (int)(i / 3))]
-                  [(32 - 1) & (x - 1 + i % 3)]) {
+               if (this.current_state[(32 - 1) & (y - 1 + Math.floor(j / 3))]
+                  [(32 - 1) & (x - 1 + j % 3)]) {
                   count++;
                }
             }
